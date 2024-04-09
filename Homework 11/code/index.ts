5b7c7552801f36a9fd8a852fb6eb489eff0bdfea
Daniel Ivtimov <danieliftimov3@gmail.com>
2024-04-09 04:22:26 +0200

    Homework

---
+++ Homework 11/code/index.ts
@@ -0,0 +1,68 @@
+interface IMovie{
+    id: number,
+    title: string,
+    duration: number,
+    genre: string,
+    hasWonOscar?: boolean;
+}
+class Movie implements IMovie{
+    id: number;
+    title: string;
+    duration: number;
+    genre: string;
+    hasWonOscar?: boolean;
+    constructor(id: number, title: string, duration: number, genre: string, hasWonOscar?: boolean){
+        this.id = id;
+        this.title = title;
+        this.duration = duration;
+        this.genre = genre;
+        this.hasWonOscar = hasWonOscar;
+    }
+}
+class MovieLibrary{
+    movies: IMovie[];
+    constructor(){
+        this.movies = [];
+    }
+    addMovie(movie: IMovie): void{
+        this.movies.push(movie);
+    }
+    printMovies(genre: string = "action"): string[]{
+        const checkinghMovies = this.movies.filter(movie => movie.genre === genre);
+        const moviesPrinting  = checkinghMovies.map(movie => movie.title);
+        console.log(moviesPrinting);
+        return moviesPrinting;
+    }
+    static movieDetails(movie: Movie): string{
+        return `The name of movie is: ${movie.title} and his genre is: ${movie.genre}`;
+    }
+}
+const firstMovie = new Movie(1, "The Lord of the Rings: The Fellowship of the Ring", 178, "fantasy", true);
+const secondMovie = new Movie(2, "Harry Potter and the Philosopher's Stone", 152, "fantasy", true);
+const thirdMovie = new Movie(3, "The Hobbit: An Unexpected Journey", 169, "fantasy", false);
+const library = new MovieLibrary();
+library.addMovie(firstMovie);
+library.addMovie(secondMovie);
+library.addMovie(thirdMovie);
+console.log("All movies:");
+library.printMovies("fantasy");
+
+// Za ovoj bonus upotrebiv i void za tip kako sto zborevme na predavanjeto 
+// Isto taka za ovoj bonus propertinjata ne gi napraviv da bidat private
+// bidejki imav problem , nemozev nikako da ja pristapam nivnata vrednost 
+// postojano dobivav greska , pa za da ne zakocam prodolziv vaka da go pravam ova 
+// Probav isto taka da upotrebam getters i setters za da gi pristapam kako od kodot od predavanjeto
+// getId():number{             // vaka pisav za getter
+//     return this.id;
+// }
+// setId(value: number){       // vaka pisav za setter
+//     this.id = id;
+// }
+// Pa ako bi mozele da mi pokazite kako mozam da go resam ova. :) :D 
+
+
+
+
+
+
+
